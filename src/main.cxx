690065f94387714995e1612ed32c89954cd7a8aa
Naifcan Başçı <naifcanbasci@gmail.com>
2025-07-12 19:44:02 +0300

    Now it draws a triangle :)

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,3 @@
-#include<glad/glad.h>
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -7,6 +5,40 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
+#include <g3d/shader.hxx>
+#include <g3d/program.hxx>
+#include <g3d/model.hxx>
+
+const char *vertexShaderSource = R"(
+	#version 330 core
+
+	layout(location = 0) in vec2 aPos;         // 2D position
+	layout(location = 1) in vec3 aColor;       // RGB color
+
+	out vec3 vColor;  // Pass color to fragment shader
+
+	void main()
+	{
+		gl_Position = vec4(aPos, 0.0, 1.0); // convert to 4D vec
+		// gl_Position.z = 5;
+		// vColor = aColor;  // pass through to fragment
+		vColor = vec3(1,0,0);
+	}
+
+)";
+
+const char *fragmentShaderSource = R"(
+#version 330 core
+
+in vec3 vColor;          // Interpolated from vertex shader
+out vec4 FragColor;      // Output color
+
+void main()
+{
+    FragColor = vec4(vColor, 1.0); // RGBA: color + full opacity
+}
+
+)";
 
 int main(void) {
 	glfwInit();
@@ -28,12 +60,26 @@ int main(void) {
         	return -1;
 	}
 
+	Shader *vertex = Shaders::create(&vertexShaderSource, GL_VERTEX_SHADER)->compile();
+	Shader *fragment = Shaders::create(&fragmentShaderSource, GL_FRAGMENT_SHADER)->compile();
+
+	Program* program = Programs::create();
+	program->attach(vertex);
+	program->attach(fragment);
+	program->link();
+
+	Model* mmodel = Models::createCube(0,0,0);
+
 	while(!glfwWindowShouldClose(window)) {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		program->use();
+
 		glEnable(GL_DEPTH_TEST);
 		// 3D renders...
+
+		Models::drawModel(mmodel);
 		glDisable(GL_DEPTH_TEST);
 		// 2D renders, like GUI...
 		
@@ -41,6 +87,7 @@ int main(void) {
 		glfwPollEvents();
 	}
 
+	Shaders::freeAll();
 	glfwTerminate();
 	return 0;
 }
