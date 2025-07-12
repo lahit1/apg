0ad3308cd6b9bab9ee92b29ba1b408d35471a811
Naifcan Başçı <naifcanbasci@gmail.com>
2025-07-12 18:37:03 +0300

    Removed unnecessary return statements, moved Shaders::create and Shaders::freeAll functions to source file

--- include/g3d/shader.hxx
+++ include/g3d/shader.hxx
@@ -1,46 +1,26 @@
 #pragma once
 
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
 struct Shader {
 	unsigned int ptr;
 	Shader* next;
 
 	inline Shader* compile() {
-        	glCompileShader(ptr);
-        	return this;
+	        glCompileShader(ptr);
+		return this;
 	}
 
-	inline Shader* del() {
+	inline void del() {
 	        glDeleteShader(ptr);
-	        return this;
 	}
 };
 
 namespace Shaders {
 
+Shader* create(const GLchar* const* code, GLenum shaderType);
 
-inline Shader *first, *last;
-
-inline Shader* create(const GLchar* const* code, GLenum shaderType) {
-        Shader *ret = new Shader();
-	if(first == nullptr) first = last = ret;
-	else {
-		last->next = ret;
-		last = ret;
-	}
-
-        ret->ptr = glCreateShader(shaderType);
-        glShaderSource(ret->ptr, 1, code, NULL);
-        return ret;
-}
-
-inline void freeAll() {
-	Shader *cur=first, *ref;
-	while(cur != nullptr) {
-		ref = cur;
-		cur = ref->next;
-		ref->del(); // Delete opengl object
-		delete ref;
-	};
-}
+void freeAll();
 
 }
--- src/g3d/shader.cxx
+++ src/g3d/shader.cxx
@@ -2,3 +2,32 @@
 #include<GLFW/glfw3.h>
 
 #include<g3d/shader.hxx>
+
+namespace Shaders {
+
+Shader *first, *last;
+
+Shader* create(const GLchar* const* code, GLenum shaderType) {
+        Shader *ret = new Shader();
+        if(first == nullptr) first = last = ret;
+        else {
+                last->next = ret;
+                last = ret;
+        }
+
+        ret->ptr = glCreateShader(shaderType);
+        glShaderSource(ret->ptr, 1, code, NULL);
+        return ret;
+}
+
+void freeAll() {
+        Shader *cur=first, *ref;
+        while(cur != nullptr) {
+                ref = cur;
+                cur = ref->next;
+                ref->del(); // Delete opengl object
+                delete ref;
+        }
+}
+
+}
