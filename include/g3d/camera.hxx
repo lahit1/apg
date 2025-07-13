7959b8a8dd59c0d8c12068abeadb6d168d929097
lahit1 <naifcanbasci@gmail.com>
2025-07-13 19:56:14 +0000

    Removed unnecessary definition(vec2)

--- include/g3d/camera.hxx
+++ include/g3d/camera.hxx
@@ -3,16 +3,16 @@
 #include<glm/glm.hpp>
 
 #define LIMIT_X_ANGLE glm::radians(75)
-#define vec2 glm::vec2
+
 #define vec3 glm::vec3
 #define mat4 glm::mat4
 
 struct Camera {
 
-	vec3 pos;
-	mat4 rot, view, dist;
+	vec3 pos, dist;
+	mat4 rot, view;
 
-	float rotx; // rot can be just a vec2 with manual calculations, may be i do it later ;)
+	float rotx;
 
 	void rotate(float radians, vec2 scale); // Scale relative to screen cursor position and screen size
 	void updateView();
