c4144dd643c023def06a883560749944b8b58898
lahit1 <naifcanbasci@gmail.com>
2025-07-13 19:37:22 +0000

    Added Camera::updateView Camera::updateProjection functions

--- include/g3d/camera.hxx
+++ include/g3d/camera.hxx
@@ -10,9 +10,11 @@
 struct Camera {
 
 	vec3 pos;
-	mat4 rot, view, padd;
+	mat4 rot, view, dist;
 
 	float rotx; // rot can be just a vec2 with manual calculations, may be i do it later ;)
 
 	void rotate(float radians, vec2 scale); // Scale relative to screen cursor position and screen size
+	void updateView();
+	void updateProjection();
 };
