3f3fa6476624621c4d4706ae865a3cb15811f12d
lahit1 <naifcanbasci@gmail.com>
2025-07-13 19:26:05 +0000

    Changed LIM_X_ANGLE value to radians

--- include/g3d/camera.hxx
+++ include/g3d/camera.hxx
@@ -2,7 +2,7 @@
 
 #include<glm/glm.hpp>
 
-#define LIMIT_X_ANGLE 75
+#define LIMIT_X_ANGLE glm::radians(75)
 #define vec2 glm::vec2
 #define vec3 glm::vec3
 #define mat4 glm::mat4
@@ -10,7 +10,7 @@
 struct Camera {
 
 	vec3 pos;
-	mat4 rot, view;
+	mat4 rot, view, padd;
 
 	float rotx; // rot can be just a vec2 with manual calculations, may be i do it later ;)
 
