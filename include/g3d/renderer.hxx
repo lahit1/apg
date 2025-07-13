da9ce72e7bfd695e984a37cc977d018d0c30b591
lahit1 <naifcanbasci@gmail.com>
2025-07-13 18:16:21 +0000

    Removed unnecessary Pool and Ball struct

--- include/g3d/renderer.hxx
+++ include/g3d/renderer.hxx
@@ -5,17 +5,6 @@
 
 namespace RENDERER {
 
-struct Ball {
-	Renderable* renderable;
-	Ball* next;
-};
-
-struct Pool { // basicly a linked list :d, i love giving different names than frequent terms :)
-	Ball *start, *end;
-	inline void link(Ball* b);
-	inline void unlink(Ball* b);
-};
-
 void BEGIN(Camera* p);
 void RENDER(void);
 void END(void);
--- src/g3d/renderer.cxx
+++ src/g3d/renderer.cxx
@@ -7,18 +7,4 @@ void BEGIN(Camera* p) {};
 void RENDER(void) {};
 void END(void) {};
 
-void Pool::link(Ball *b) {
-	if(!start) start = end = b;
-	if(end == b) return;
-	else {
-		end->next = b;
-		end = b;
-	}
-	b->next = nullptr;
-}
-
-void Pool::unlink(Ball *b) {
-	b->renderable = nullptr;
-}
-
 }
