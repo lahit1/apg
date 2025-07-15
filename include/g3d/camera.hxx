a978c0a6a5d125e5fb4dbca486a6ea0a80718b37
Naifcan Başçı <naifcanbasci@gmail.com>
2025-07-15 15:12:23 +0300

    Changed structure .d

--- include/g3d/camera.hxx
+++ include/g3d/camera.hxx
@@ -8,16 +8,20 @@
 #define vec3 glm::vec3
 #define mat4 glm::mat4
 
+
+constexpr vec3 zaxis{0, 0, 1};
+constexpr vec3 yaxis{0, 1, 0};
+constexpr vec3 xaxis{1, 0, 0};
+
 struct Camera {
 
 	Camera();
 
-	vec3 pos = vec3(0,0,0), dist = vec3(0,0,0);
-	mat4 rot = mat4(1), view, proj, vp;
-	float rotx;
+	vec3 pos = vec3(0,0,0), dist = vec3(0,0,5), up = yaxis;
+	mat4 view, proj, vp;
 
 	float fov = glm::radians(45.0f);
-	float aspect = 4 / 3;
+	float aspect = 4.0f / 3.0f;
 	float nearPlane = 0.1f;
 	float farPlane = 100.0f;
 
