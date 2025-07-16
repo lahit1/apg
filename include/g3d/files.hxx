2c4a7841148658cdf14e2deec24b1be1c57e74cc
Naifcan Başçı <naifcanbasci@gmail.com>
2025-07-16 16:26:21 +0300

    Added files header file

---
+++ include/g3d/files.hxx
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <istream>
+#include <ostream>
+#include <iostream>
+
+namespace Files {
+
+std::istream& openi(const char* filename);
+std::ostream& openo(const char* filename);
+std::iostream& openio(const char* filename);
+
+}
