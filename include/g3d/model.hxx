dbe1f51ce7624f0273000394144b55800d4fdb70
Naifcan Başçı <naifcanbasci@gmail.com>
2025-07-16 16:02:19 +0300

    Added material atribute to struct

--- include/g3d/model.hxx
+++ include/g3d/model.hxx
@@ -3,13 +3,18 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include<g3d/renderable.hxx>
+#include<g3d/material.hxx>
 #include<glm/glm.hpp>
 
 #define mat4 glm::mat4
 
+
 struct Model : public Renderable {
 	unsigned int VAO; // Buffer array (Vertices data)
 	size_t indexCount;
+
+	Material material;
+
 	mat4 matrix = mat4(1);
 };
 
