de13e0e5110fdfa58772090dc7c25a93ee626423
lahit1 <naifcanbasci@gmail.com>
2025-07-18 19:59:18 +0000

    Remove unnecessary log messages

--- src/g3d/renderer.cxx
+++ src/g3d/renderer.cxx
@@ -32,18 +32,14 @@ void DRAW(Model* m) {
 	int xorv = ~m->active_vertex_attr_arrs & active_vertex_attr_arrs;
 
         for(unsigned int i=0; i < 8; i++)
-                if(xorv & (1 << i)) {
+                if(xorv & (1 << i))
                         glDisableVertexAttribArray(i);
-                        std::cout << "Disable: " << i << std::endl;
-                }
 
         xorv = m->active_vertex_attr_arrs & ~active_vertex_attr_arrs;                                                                                           for(unsigned int i=0; i < 8; i++)
 
 	for(unsigned int i=0; i < 8; i++)
-                if(xorv & (1 << i)) {
+                if(xorv & (1 << i))
                         glEnableVertexAttribArray(i);
-                        std::cout << "Enable: " << i << std::endl;
-                }
 
 	active_vertex_attr_arrs = m->active_vertex_attr_arrs;
 
