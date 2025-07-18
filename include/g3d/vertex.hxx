74e192d63aa475b7500f50167c6d5236c975fd97
lahit1 <naifcanbasci@gmail.com>
2025-07-18 23:10:11 +0000

    Added == operator

--- include/g3d/vertex.hxx
+++ include/g3d/vertex.hxx
@@ -4,4 +4,11 @@ struct Vertex {
 	glm::vec3 pos, norm;
 	glm::vec2 texCoord;
 	glm::vec4 color; // rgba
+
+	inline bool operator==(const Vertex& other) const {
+	        return pos == other.pos &&
+        	       norm == other.norm &&
+	               texCoord == other.texCoord &&
+        	       color == other.color;
+	}
 };
