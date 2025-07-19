620bb7ff501015438b690471cf25dc65dc906a92
lahit1 <naifcanbasci@gmail.com>
2025-07-19 10:53:37 +0000

    Change variable type of arguament

--- src/utils/files.cxx
+++ src/utils/files.cxx
@@ -3,15 +3,15 @@
 
 namespace Files {
 
-std::istream* openi(char *filename) {
+std::istream* openi(const char *filename) {
 	std::ifstream* ifs = new std::ifstream(filename);
 	return ifs;
 };
-std::ostream* openo(char *filename) {
+std::ostream* openo(const char *filename) {
 	std::ofstream* ofs = new std::ofstream(filename);
         return ofs;
 };
-std::iostream* openio(char *filename) {
+std::iostream* openio(const char *filename) {
 	std::fstream* iofs = new std::fstream(filename);
         return iofs;
 };
