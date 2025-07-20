dc0883da45f7e8989f66b6982e1a3b81acad0845
lahit1 <naifcanbasci@gmail.com>
2025-07-20 21:48:54 +0000

    Add new function for hierarchical drawing(or just for Model, lol)

--- include/g3d/renderer.hxx
+++ include/g3d/renderer.hxx
@@ -2,11 +2,14 @@
 
 #include<g3d/program.hxx>
 #include<g3d/camera.hxx>
+#include<g3d/mesh.hxx>
+#include<g3d/model.hxx>
 
 namespace RENDERER {
 
 void BEGIN(Camera* c);
 void USE(Program* p);
 void DRAW(Mesh* m);
+void DRAW(Model* m);
 void END(void);
 }
--- src/g3d/renderer.cxx
+++ src/g3d/renderer.cxx
@@ -1,5 +1,4 @@
 #include<g3d/renderable.hxx>
-#include<g3d/mesh.hxx>
 #include<g3d/renderer.hxx>
 #include<glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -22,12 +21,23 @@ void USE(Program* p) {
 
 void DRAW(Mesh* m) {
 	glm::mat4 mvp = c_cam->vp;
-
 	glUniformMatrix4fv(c_p->mvpULoc_ptr, 1, GL_FALSE, glm::value_ptr(mvp));
 
         glBindVertexArray(m->VAO);
-
         glDrawElements(GL_TRIANGLES, m->indexCount, GL_UNSIGNED_INT, 0);
+}
+
+void DRAW(Model* mod) {
+	glm::mat4 mvp = c_cam->vp * mod->matrix;
+        glUniformMatrix4fv(c_p->mvpULoc_ptr, 1, GL_FALSE, glm::value_ptr(mvp));
+
+	mod->reset();
+	while(mod->isAvailable()) {
+		Mesh* m = mod->next();
+
+		glBindVertexArray(m->VAO);
+	        glDrawElements(GL_TRIANGLES, m->indexCount, GL_UNSIGNED_INT, 0);
+	}
 }
 
 void END(void) {};
