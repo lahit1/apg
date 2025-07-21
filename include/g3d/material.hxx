c47c0312d160b80ad9c99eae3dac307ae59c2705
Naifcan Başçı <naifcanbasci@gmail.com>
2025-07-21 16:14:52 +0300

    Change texture variable type "GLuint" to "Texture", remove Materials::loadTexture()

--- include/g3d/material.hxx
+++ include/g3d/material.hxx
@@ -5,20 +5,9 @@
 #include <glm/glm.hpp>
 
 struct Material {
-	GLuint diffuseTexture = 0;
-        GLuint specularTexture = 0;
-        GLuint normalTexture = 0;
+	Texture *diffuseTexture, *specularTexture, *normalTexture;
 
         glm::vec3 diffuseColor = glm::vec3(1.0f);
         glm::vec3 specularColor = glm::vec3(1.0f);
         float shininess = 32.0f;
 };
-
-
-namespace Materials {
-
-
-GLuint loadTexture(std::istream& s);
-
-
-}
