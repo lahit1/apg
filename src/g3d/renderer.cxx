1862595c7618d205835d4396c026312dfe17f365
Naifcan Başçı <naifcanbasci@gmail.com>
2025-07-22 16:05:28 +0300

    Integrate to new program struct

--- src/g3d/renderer.cxx
+++ src/g3d/renderer.cxx
@@ -18,18 +18,26 @@ void USE(Program* p) {
 	glUseProgram(p->ptr);
 	c_p = p;
 }
-
+/*
 void DRAW(Mesh* m) {
-	glm::mat4 mvp = c_cam->vp;
-	glUniformMatrix4fv(c_p->mvpULoc_ptr, 1, GL_FALSE, glm::value_ptr(mvp));
+	glUniformMatrix4fv(c_p->vpULoc_ptr, 1, GL_FALSE, glm::value_ptr(c_cam->vp));
 
         glBindVertexArray(m->VAO);
         glDrawElements(GL_TRIANGLES, m->indexCount, GL_UNSIGNED_INT, 0);
 }
+*/
 
 void DRAW(Model* mod) {
-	glm::mat4 mvp = c_cam->vp * mod->matrix;
-        glUniformMatrix4fv(c_p->mvpULoc_ptr, 1, GL_FALSE, glm::value_ptr(mvp));
+        glUniformMatrix4fv(c_p->vpULoc_ptr, 1, GL_FALSE, glm::value_ptr(c_cam->vp));
+        glUniformMatrix4fv(c_p->modelULoc_ptr, 1, GL_FALSE, glm::value_ptr(mod->matrix));
+        glUniformMatrix4fv(c_p->normalULoc_ptr, 1, GL_FALSE, glm::value_ptr(glm::transpose(glm::inverse(mod->matrix))));
+
+	Material* mat = mod->material;
+        glUniform3fv(c_p->materialDiffuseLoc_ptr, 1, glm::value_ptr(mat->diffuseColor));
+        glUniform3fv(c_p->materialSpecularLoc_ptr, 1, glm::value_ptr(mat->specularColor));
+        glUniform1f(c_p->materialShininessLoc_ptr, mat->shininess);
+
+	glUniform3fv(c_p->viewPosULoc_ptr, 1, glm::value_ptr(c_cam->pos));
 
 	mod->reset();
 	while(mod->isAvailable()) {
