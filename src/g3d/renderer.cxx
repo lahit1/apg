61127e7fbe8c160a5858513330b7586308c3dcf3
Naifcan Başçı <naifcanbasci@gmail.com>
2025-07-25 18:07:48 +0300

    Added if statements for security and less cpu->gpu transmissions

--- src/g3d/renderer.cxx
+++ src/g3d/renderer.cxx
@@ -33,9 +33,25 @@ void DRAW(Model* mod) {
         glUniformMatrix4fv(c_p->normalULoc_ptr, 1, GL_FALSE, glm::value_ptr(glm::transpose(glm::inverse(mod->matrix))));
 
 	Material* mat = mod->material;
-        glUniform3fv(c_p->materialDiffuseLoc_ptr, 1, glm::value_ptr(mat->diffuseColor));
-        glUniform3fv(c_p->materialSpecularLoc_ptr, 1, glm::value_ptr(mat->specularColor));
-        glUniform1f(c_p->materialShininessLoc_ptr, mat->shininess);
+	if(c_p->materialDiffuseCLoc_ptr)
+	        glUniform3fv(c_p->materialDiffuseCLoc_ptr, 1, glm::value_ptr(mat->diffuseColor));
+	if(c_p->materialSpecularCLoc_ptr)
+	        glUniform3fv(c_p->materialSpecularCLoc_ptr, 1, glm::value_ptr(mat->specularColor));
+	if(c_p->materialShininessLoc_ptr)
+	        glUniform1f(c_p->materialShininessLoc_ptr, mat->shininess);
+
+	if(c_p->materialDiffuseTLoc_ptr) {
+                glUniform1i(c_p->materialDiffuseTLoc_ptr, 0);
+		mat->diffuseTexture.bind(0);
+	}
+        if(c_p->materialSpecularTLoc_ptr) {
+                glUniform1i(c_p->materialSpecularTLoc_ptr, 1);
+		mat->specularTexture.bind(1);
+	}
+        if(c_p->materialNormalTLoc_ptr) {
+                glUniform1i(c_p->materialNormalTLoc_ptr, 2);
+		mat->normalTexture.bind(2);
+	}
 
 	glUniform3fv(c_p->viewPosULoc_ptr, 1, glm::value_ptr(c_cam->pos));
 
