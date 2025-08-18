0ebc3efa47b18363bee28fb19db7eeea9e8898b8
Naifcan Başçı <naifcanbasci@gmail.com>
2025-08-18 17:03:29 +0300

    Store uniforms in unordered_map, now it's more functional when we load .mtl files

--- include/g3d/material.hxx
+++ include/g3d/material.hxx
@@ -2,13 +2,45 @@
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <g3d/texture.hxx>
+#include <unordered_map>
 
+struct uniformfv {
+	void (*binder_funcfv)(
+		GLint location,
+		GLsizei count,
+		const GLfloat *value);
+
+	GLfloat *value;
+
+	inline uniformfv() = default;
+
+	inline uniformfv(void (*binder_funcfv)(
+		GLint location,
+		GLsizei count,
+		const GLfloat *value), GLfloat *value) : value(value), binder_funcfv(binder_funcfv) {}
+};
 
 struct Material {
-	Texture diffuseTexture, specularTexture, normalTexture;
+	std::unordered_map<GLuint, uniformfv> uniforms_f;
+	std::unordered_map<GLuint, Texture> textures;
 
-        glm::vec3 diffuseColor = glm::vec3(1.0f);
-        glm::vec3 specularColor = glm::vec3(1.0f);
-        float shininess = 32.0f;
+	inline Material* add(GLuint target, glm::vec3 value) {
+		uniforms_f[target] = { glUniform3fv, glm::value_ptr(value) };
+		return this;
+	}
+	inline Material* add(GLuint target, glm::vec2 value) {
+		uniforms_f[target] = uniformfv{ glUniform2fv, glm::value_ptr(value) };
+		return this;
+	}
+	inline Material* add(GLuint target, float value) {
+		uniforms_f[target] = uniformfv{ glUniform1fv, new float(value) };
+		return this;
+	}
+
+	inline Material* add(GLuint target, Texture tex) {
+		textures[target] = tex;
+		return this;
+	}
 };
