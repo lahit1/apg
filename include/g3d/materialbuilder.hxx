2dfbec71dc5028768e8000f2be6bcf3afb171975
Naifcan Başçı <naifcanbasci@gmail.com>
2025-08-19 13:56:39 +0300

    Add MaterialBuilder class for building easier .mtl loader soon

---
+++ include/g3d/materialbuilder.hxx
@@ -0,0 +1,52 @@
+#include <g3d/material.hxx>
+#include <g3d/program.hxx>
+
+class MaterialBuilder {
+
+	std::unordered_map<std::string, std::string> reps;
+	std::unordered_map<std::string, uniformfv> loads_uf;
+	std::unordered_map<std::string, Texture> loads_tex;
+
+public:
+        inline MaterialBuilder* add(std::string target, glm::vec3 value) {
+                loads_uf[target] = { glUniform3fv, glm::value_ptr(value) };
+                return this;
+        }
+        inline MaterialBuilder* add(std::string target, glm::vec2 value) {
+                loads_uf[target] = uniformfv{ glUniform2fv, glm::value_ptr(value) };
+                return this;
+        }
+        inline MaterialBuilder* add(std::string target, float value) {
+                loads_uf[target] = uniformfv{ glUniform1fv, new float(value) };
+                return this;
+        }
+
+        inline MaterialBuilder* add(std::string target, Texture tex) {
+                loads_tex[target] = tex;
+                return this;
+        }
+
+	inline MaterialBuilder* rep(std::string first, std::string second) {
+		reps[first] = second;
+		return this;
+	}
+
+	inline Material* build(Program* p) {
+		Material *mat = new Material();
+		for(auto u: loads_uf) {
+			std::string u_name;
+			if(reps.find(u.first) != reps.end())
+				u_name = reps[u.first];
+			else u_name = u.first;
+			mat->uniforms_f[glGetUniformLocation(p->ptr, u_name.c_str())] = u.second;
+		}
+		for(auto tex: loads_tex) {
+			std::string u_name;
+			if(reps.find(tex.first) != reps.end())
+                                u_name = reps[tex.first];
+                        else u_name = tex.first;
+			mat->textures[glGetUniformLocation(p->ptr, u_name.c_str())] = tex.second;
+		}
+		return mat;
+	}
+};
